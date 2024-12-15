a063d751d7a250199e7ef829691e93c2f3827424
Klotzkopf <marcojotonn@gmail.com>
2024-12-15 12:28:08 +0100

    Changed functions to exclude numbers and special letters

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     words = get_num_words(text)
-    print(text)
-    print(f"the counted number of words is {words}")
+    original_text = text
+    cleaned_text = exclude_special_chars(original_text)
+    counted_characters =  count_characters(cleaned_text)
+    print(f"-- Facts about the text {book_path}--")
+    print(f"The counted number of words is {words}.")
+    print(f"Characters counted. Results are: {counted_characters}")
+
 
 def get_text(book_path):
     with open("books/frankenstein.txt") as f:
@@ -12,6 +17,24 @@ def get_text(book_path):
 
 def get_num_words(text):
     words = text.split()
-    return len(words)      
- 
+    return len(words)  
+
+def exclude_special_chars(text):
+    filtered_text = filter(str.isalpha, text)
+    return ''.join(filtered_text)
+
+
+def count_characters(cleaned_text): 
+    chars = {}
+    for c in cleaned_text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return dict(sorted(chars.items()))
+
+def sort_on(counted_characters):
+    return counted_characters["num"]
+
 main()
